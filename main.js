ad1e62e605dff9d4c5928427e552aa89e44e68d1
fruityclub <remixlille@free.fr>
2013-06-06 16:46:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -17,16 +17,6 @@
        under the License.
 */
 
-var deviceInfo = function() {
-    document.getElementById("platform").innerHTML = device.platform;
-    document.getElementById("version").innerHTML = device.version;
-    document.getElementById("uuid").innerHTML = device.uuid;
-    document.getElementById("name").innerHTML = device.name;
-    document.getElementById("width").innerHTML = screen.width;
-    document.getElementById("height").innerHTML = screen.height;
-    document.getElementById("colorDepth").innerHTML = screen.colorDepth;
-};
-
 var getLocation = function() {
     var suc = function(p) {
         alert(p.coords.latitude + " " + p.coords.longitude);
@@ -155,11 +145,4 @@ function toggleCompass() {
             alert('Compass Error: ' + e.code);
         }, options);
     }
-}
-
-function init() {
-    // the next line makes it impossible to see Contacts on the HTC Evo since it
-    // doesn't have a scroll button
-    // document.addEventListener("touchmove", preventBehavior, false);
-    document.addEventListener("deviceready", deviceInfo, true);
 }
