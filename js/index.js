1437541ea4aadcd0ae682de17c281c60972d1eb7
fruityclub <remixlille@free.fr>
2013-06-14 17:14:07 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,6 +8,14 @@ $(document).ready(fn_show_Connexion1);
 $(document).on('submit', '#checkuser', fn_submitconnexion);
 $(document).on('vclick', '#submitlog', fn_submitconnexion);
 
+$(document).on('pageshow', '#config', fn_show_config);
+$(document).on('vclick', '#btn_autoLogConfig', fn_del_autolog);
+$(document).on('vclick', '#submit_config', fn_submit_config);
+$(document).on('vclick', '#btn_confirmConfig', fn_click_btn_confirmConfig);
+
+$(document).on('pageshow', '#ShoutboxList', fn_show_ShoutboxList);
+$(document).on('vclick', 'li.LIShoutboxList', fn_click_LIShoutboxList);
+
 // Functions
 function onDeviceReady() {
 	var deviceReady = true;	
@@ -221,4 +229,249 @@ function shoutboxAccess(returndata) {
 		$('#content').load('shoutboxslist.html');
 	}
 
+}
+
+function vibrateOnNewMEssages() {
+  if (typeof(deviceReady) != "undefined" && deviceReady !== null) {
+		navigator.notification.vibrate(1000);
+	}
+}
+
+function get_shoutbox_infos(id_shoutbox) {
+
+	var id_shoutbox = window.localStorage.getItem("id_shoutbox");
+	var id_member = window.localStorage.getItem("id_member");
+	var auth_token = window.localStorage.getItem("auth_token");
+
+	$.ajax({url: 'https://www.fruityclub.net/api/index.php/shoutbox/shoutboxinfos',
+		type: 'post',
+		data: {'auth_token': auth_token, 'id_member': id_member, 'id_shoutbox': id_shoutbox},
+		async: true,
+		beforeSend: function() {
+			$.mobile.loading( 'show', { theme: "b", text: "Recuperation d'informations ...", textVisible: true });
+		},
+		complete: function() {
+			$.mobile.loading( 'hide' );
+		},
+		success: function (responseText) {
+			$("#ShoutboxTitle").html( responseText.name );
+			if ( responseText.warning != '' ) { 
+				$("#shoutbox_warning").show(); $("#shoutbox_warning").html( responseText.warning ); 
+			} else {
+				$("#shoutbox_warning").hide();
+			}
+		},
+		error: function (responseText) {
+
+		}
+	});
+
+}
+
+function refresh_shoutlist(loading) {
+
+  loading = loading || false;
+
+	// loading = true si premier refresh
+	var id_shoutbox = window.localStorage.getItem("id_shoutbox");
+	var id_member = window.localStorage.getItem("id_member");
+	var auth_token = window.localStorage.getItem("auth_token");
+	var last_update1 = $("#last_update").val();
+
+	var vibration_msg = window.localStorage.getItem("vibration_msg");
+	var msg_size = window.localStorage.getItem("msg_size");
+
+	// get shoutbox list
+	$.ajax({url: 'https://www.fruityclub.net/api/index.php/shoutbox/shoutlist',
+		type: 'post',
+		data: {'auth_token': auth_token, 'id_member': id_member, 'id_shoutbox': id_shoutbox, 'last_update': last_update1},
+		async: true,
+		beforeSend: function() {
+			if ( loading == true ) { $.mobile.loading( 'show', { theme: "b", text: "Mise à jour des messages ...", textVisible: true }); }
+		},
+		complete: function() {
+			if ( loading == true ) { $.mobile.loading( 'hide' ); $("#loadingMessages").popup("close"); }
+			$('#ULShoutList').listview('refresh');
+
+			var last_update2 = $('#ULShoutList li').last().attr('data-name');
+			$("#last_update").val(last_update2);
+
+			$(".linkInMessage").each(function() {
+
+			  var hrefUrl = $(this).attr('href');
+			  var hrefInfos = $.mobile.path.parseUrl(hrefUrl);
+
+			  var hrefHost = hrefInfos.hostname;
+			  $(this).text(hrefHost);
+
+			});
+		},
+		success: function (responseText) {
+			if ( loading == true ) { $("#ULShoutList").empty(); }
+			if (typeof(responseText) != "undefined" && responseText !== null) {
+				if ( responseText.length > 0) {	
+					var shoutsList = '';
+					$.each( responseText, function( i, item ) {
+						shoutsList += '<li class="LIShoutList" data-name="' + item.log_time + '" data-inset="true">';
+						shoutsList += '<img src="' + item.avatar + '" width="76" height="76" style="border: 2px solid white;" />';						
+						shoutsList += '<b style="color:' + item.name_color + '; text-shadow:0px 0px 2px ' + item.name_color_glow + ';">' + item.member_name + '</b><br /><span style="font-size:' + msg_size + 'px;">' + item.body + '</span>';
+						shoutsList += '</li>';
+					});
+					$("#ULShoutList").append( shoutsList );
+					if ( vibration_msg == 1 ) { 
+						vibrateOnNewMEssages(); 
+					}
+				}
+			}
+		},
+		error: function (responseText) {             
+			$.mobile.loading( 'hide' );
+			$.mobile.changePage($('#Connexion1'));
+			$('#erreur_connexion').show();
+		}
+	});
+
+}
+
+function startRefresh(firstRefresh) {
+
+  firstRefresh = firstRefresh || false;
+
+	refresh_shoutlist(firstRefresh);	
+	var refreshIntervalId = setTimeout(startRefresh, 30000);
+
+	// on conserve l'id du refreshinterval pour utilisation future
+	window.localStorage.setItem("refreshIntervalId", refreshIntervalId);	
+}
+
+function stopRefresh() {
+	if (typeof(window.localStorage.getItem("refreshIntervalId")) != "undefined" && window.localStorage.getItem("refreshIntervalId") !== null) {
+		var refreshIntervalId = window.localStorage.getItem("refreshIntervalId");
+		clearInterval(refreshIntervalId);
+	}
+
+}
+
+function fn_show_config() {
+
+  stopRefresh();
+
+	var vibration_msg = window.localStorage.getItem("vibration_msg");
+	var msg_size = window.localStorage.getItem("msg_size");
+
+	// mets valeurs par défaut si existe	
+	if ( !vibration_msg ) { vibration_msg = 0; }
+	if ( !msg_size ) { msg_size = 12; }
+
+	$('input:radio[name="msg_size"]').filter('[value="' + msg_size + '"]').next().click();
+	$('#vibration_msg').val(vibration_msg);
+	$('#vibration_msg').slider('refresh');
+
+}
+
+function fn_del_autolog() {
+	window.localStorage.removeItem("email");
+	window.localStorage.removeItem("password");
+	window.localStorage.removeItem("secretkey");
+
+	$("#popup_confirmAutolog").popup("open");
+}
+
+function fn_submit_config() {
+
+	var vibration_msg = $('#vibration_msg').val();
+	var msg_size = $("input[name=msg_size]:checked").val();
+
+	window.localStorage.setItem("vibration_msg", vibration_msg);
+	window.localStorage.setItem("msg_size", msg_size);	
+
+	$("#popup_confirmConfig").popup("open");
+
+}
+
+function fn_click_btn_confirmConfig() {
+
+	$("#popup_confirmConfig").popup("close");
+
+	if (typeof(window.localStorage.getItem("id_shoutbox")) != "undefined" && window.localStorage.getItem("id_shoutbox") !== null) {
+		$.mobile.changePage($('#ShoutList'));
+	} else {	
+		$.mobile.changePage($('#ShoutboxList'));
+	}	
+
+}
+
+function fn_show_ShoutboxList(event, data) {
+
+  var id_member = window.localStorage.getItem("id_member");
+	var real_name = window.localStorage.getItem("real_name");
+	var avatar = window.localStorage.getItem("avatar");
+	var id_group = window.localStorage.getItem("id_group");
+	var isvip = window.localStorage.getItem("isvip");
+	var isadmin = window.localStorage.getItem("isadmin");
+	var unread_messages = window.localStorage.getItem("unread_messages");
+	var name_color = window.localStorage.getItem("name_color");
+	var name_color_glow = window.localStorage.getItem("name_color_glow");
+	var money = window.localStorage.getItem("money");
+	var posts = window.localStorage.getItem("posts");
+	var auth_token = window.localStorage.getItem("auth_token");
+	var vibration_msg = window.localStorage.getItem("vibration_msg");
+	var msg_size = window.localStorage.getItem("msg_size");
+
+	stopRefresh();
+	$("#last_update").val('0');
+
+	var group_title = 'Membre';
+	var vip_title = 'Non VIP';
+	if ( isadmin == "true" ) { group_title = "Administrateur"; } else if ( id_group == 2 ) { group_title = "Mod&eacute;rateur"; }
+	if ( isvip == "true" ) { vip_title = "VIP"; }
+
+	$('.MemberInfos').empty();
+
+	$('#ULMemberInfos1').html('<img src="' + avatar + '" height="76" style="border: 2px solid white;" />');
+	$('#ULMemberInfos2').html('<b style="color:' + name_color + '; text-shadow:0px 0px 2px ' + name_color_glow + ';">' + real_name + '</b><br />' + group_title + ' ' + vip_title + '<br />' + unread_messages + ' message(s) non lu(s)<br />' + posts + ' message(s)<br />' + money + ' point(s)');
+
+	var nbShoutboxs = $("#ULShoutboxList li").size();
+
+	if ( data.prevPage.attr("id") != "ShoutList" || nbShoutboxs == 0 ) {
+
+		// get shoutbox list
+		$.ajax({url: 'https://www.fruityclub.net/api/index.php/shoutbox/shoutboxlist',
+			type: 'post',
+			data: {'auth_token': auth_token, 'id_member': id_member},
+			async: true,
+			beforeSend: function() {
+				$.mobile.loading( 'show', { theme: "b", text: "Affichage des ShoutBoxs ...", textVisible: true });
+			},
+			complete: function() {
+				$.mobile.loading( 'hide' );
+				$('#ULShoutboxList').listview('refresh');
+			},
+			success: function (responseText) {
+				$("#ULShoutboxList").empty();
+				$('#ULShoutboxList').listview();
+				var shoutboxsList = '';
+				$.each( responseText, function( i, item ) {
+					shoutboxsList += '<li data-icon="arrow-r" class="LIShoutboxList" data-name="' + item.id_shoutbox + '"><a href="#">';
+					shoutboxsList += item.name;
+					shoutboxsList += '</a></li>';
+				});
+				$("#ULShoutboxList").append( shoutboxsList );
+
+			},
+			error: function (responseText) {             
+				$.mobile.loading( 'hide' );
+				$('#erreur_connexion').show();
+			}
+		});
+
+	}
+
+}
+
+function fn_click_LIShoutboxList() {
+	console.log('fn_click_LIShoutboxList');
+	var id_shoutbox = $(this).attr('data-name');
+	window.localStorage.setItem("id_shoutbox", id_shoutbox);
+	$.mobile.changePage($('#ShoutList'));	
 }
