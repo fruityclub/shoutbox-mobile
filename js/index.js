9828e04e4d94010a860bc337935b5b802916d58c
fruityclub <remixlille@free.fr>
2013-06-15 14:08:55 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -82,7 +82,7 @@ function fn_show_Connexion1() {
 		$('#content').load('html/connexion.html');	
 	}
 	
-	$('#autolog').bootstrapSwitch();
+	$('.switch').bootstrapSwitch();
 }
 
 function fn_submitconnexion(e) {
